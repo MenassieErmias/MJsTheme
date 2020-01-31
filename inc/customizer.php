30ae8fea6d2aa7636a1d997f3b49f823ce05e38c
MenassieErmias <37955306+MenassieErmias@users.noreply.github.com>
2020-01-31 11:39:29 +0300

    add menu

--- inc/customizer.php
+++ inc/customizer.php
@@ -50,6 +50,6 @@ function mjstheme_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function mjstheme_customize_preview_js() {
-	wp_enqueue_script( 'mjstheme-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'mjstheme-customizer', get_template_directory_uri() . '/dist/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'mjstheme_customize_preview_js' );
