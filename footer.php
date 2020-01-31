5d9d7fa252960bc80a00cfaf32f7bcc3d58a2ded
MenassieErmias <37955306+MenassieErmias@users.noreply.github.com>
2020-02-01 00:26:52 +0300

    sidebar added

--- footer.php
+++ footer.php
@@ -13,18 +13,18 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
+	<footer id="colophon" class="site-footer row">
+		<div class="site-info col-md-12">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mjstheme' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'mjstheme' ), 'WordPress' );
+				printf( esc_html__( 'Proudly Powered by %s', 'mjstheme' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'mjstheme' ), 'mjstheme', '<a href="http://underscores.me/">Menassie Ermias</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'MJsTheme' ), 'mjstheme', '<a href="http://underscores.me/">Menassie Ermias</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
