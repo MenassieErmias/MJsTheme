ac5c7cd906f0035d4cb173dbbea46acf66b7157b
MenassieErmias <37955306+MenassieErmias@users.noreply.github.com>
2020-02-03 00:55:30 +0300

    remove unnecessary files

--- assets/js/slick-init.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($){
-	$('.team-items').slick({ //add in your correct containing element
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  autoplay: true,
-	  autoplaySpeed: 2000,
-	});
-});
--- inc/widgets.php
+++ inc/widgets.php
@@ -15,14 +15,6 @@ function meneth_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
-	register_sidebar( array(
-		'name'          => esc_html__( 'left Sidebar', 'meneth' ),
-		'id'            => 'left-sidebar',
-		'description'   => esc_html__( 'Add widgets here.', 'meneth' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+	
 }
 add_action( 'widgets_init', 'meneth_widgets_init' );
